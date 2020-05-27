f8cfbb7a9f9fecf9de5fea78f72fa285177fd98d
Vasiliy Pupkin <vpupkin@company.com>
2020-05-27 17:03:58 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -3,14 +3,5 @@ $('.welcome-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: true,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 415,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  arrows: false
 });
